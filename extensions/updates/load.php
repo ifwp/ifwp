72804c505016f3b4bb7ee1d3bb1de167fc2b82a6
Luis del Cid <luisdelcid@Luiss-MacBook.local>
2020-09-24 11:09:22 -0600

    IFWP

--- extensions/updates/load.php
+++ extensions/updates/load.php
@@ -2,44 +2,21 @@
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// classes
-//
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-if(!class_exists('IFWP_Updates')){
-    class IFWP_Updates {
-
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        //
-        // static public
-        //
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        static public function load(){
-            require_once(plugin_dir_path(__FILE__) . 'plugin-update-checker-4.9/plugin-update-checker.php');
-        }
-
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    }
-}
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//
 // functions
 //
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 if(!function_exists('ifwp_build_update_checker')){
     function ifwp_build_update_checker(...$args){
-        return Puc_v4_Factory::buildUpdateChecker(...$args);
+        ifwp_maybe_load_plugin_update_checker();
+        return \Puc_v4_Factory::buildUpdateChecker(...$args);
     }
 }
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//
-// loader
-//
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-IFWP_Updates::load();
+if(!function_exists('ifwp_maybe_load_plugin_update_checker')){
+    function ifwp_maybe_load_plugin_update_checker(){
+		if(!class_exists('\Puc_v4_Factory')){
+			require_once(plugin_dir_path(__FILE__) . 'plugin-update-checker-4.9/plugin-update-checker.php');
+		}
+    }
+}
